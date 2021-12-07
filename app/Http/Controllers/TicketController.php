b86ba7512624a60733a3ec5343ce9a22a9f571d6
jowen-abucejo <jowennavalabucejo24@gmail.com>
2021-12-07 11:15:06 +0800

    -substitute date_format to to_char for postgresql

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -132,8 +132,9 @@ class TicketController extends Controller
                 'datetime_of_apprehension', '>=', now()->startOfMonth()->toDateString()
             )->where('datetime_of_apprehension', '<=', now()->endOfMonth()->toDateString()
             )->groupBy('day')->orderBy('day', 'ASC')->get(array(
-                    DB::raw('date_format(datetime_of_apprehension, "%b-%d") as day'),
-                    DB::raw('COUNT(*) as "total_tickets"')
+                // DB::raw('date_format(datetime_of_apprehension, "%b-%d") as day'),
+                DB::raw('to_char(datetime_of_apprehension, "Mon-DD") as day'),
+                DB::raw('COUNT(*) as "total_tickets"')
                 )
             );
             return response()->json([
@@ -148,7 +149,8 @@ class TicketController extends Controller
                 'datetime_of_apprehension', '<', Carbon::createFromFormat('Y-m-d', $request->year.'-'.$next_month.'-01')
             )->groupBy('day')->orderBy('day', 'ASC')->get(
                 array(
-                    DB::raw('date_format(datetime_of_apprehension, "%b-%d") as day'),
+                    // DB::raw('date_format(datetime_of_apprehension, "%b-%d") as day'),
+                    DB::raw('to_char(datetime_of_apprehension, "Mon-DD") as day'),
                     DB::raw('COUNT(*) as "total_tickets"')
                 )
             );

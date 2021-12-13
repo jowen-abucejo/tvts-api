b76b1258c6e23726ffc8cd335b4df4606cf1a059
jowen-abucejo <jowennavalabucejo24@gmail.com>
2021-12-13 09:35:08 +0800

    -fixed sms

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -65,12 +65,18 @@ class TicketController extends Controller
             $violation_ids = explode(',',$request->committed_violations);
             $ticket->violations()->attach($violation_ids);
             
-            Nexmo::message()->send([
-                'to'=>"63$ticket->violator->mobile_number",
-                'from'=>'Naic PNP/NTMO',
-                'text'=>"Citation Ticket $ticket->ticket_number was issued to you. Please appear at the Naic PNP/NTMO  within 72 hours to answer the stated charges. 
-                Failing to settle your case within 15 days from date of apprehension will result to the suspension/revocation of your license.",
-            ]);
+            $err='';
+            try {
+                Nexmo::message()->send([
+                    'to'=>"63"+$ticket->violator->mobile_number,
+                    'from'=>'Naic PNP/NTMO',
+                    'text'=>"Citation Ticket $ticket->ticket_number was issued to you. Please appear at the Naic PNP/NTMO  within 72 hours to answer the stated charges. 
+                    Failing to settle your case within 15 days from date of apprehension will result to the suspension/revocation of your license.",
+                ]);
+            } catch (\Throwable $th) {
+                $err = $th->getMessage();
+            }
+           
             return new TicketResource($ticket);
         } else {
             return null;

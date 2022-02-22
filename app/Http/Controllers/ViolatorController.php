8b747c1c492f1b7525982fc351a4e38c76b45e61
jowen-abucejo <jowennavalabucejo24@gmail.com>
2022-02-22 21:05:03 +0800

    -

--- app/Http/Controllers/ViolatorController.php
+++ app/Http/Controllers/ViolatorController.php
@@ -23,14 +23,14 @@ class ViolatorController extends Controller
         $order = ($request->order)?? 'ASC';
         $search = ($request->search)?? '';
         $like = (env('DB_CONNECTION') == 'pgsql') ? 'ILIKE' : 'LIKE';
-        $full_name_query = DB::raw("concat_ws(' ', `last_name`, `first_name`, `middle_name`) as name");
+        $full_name_query = DB::raw("concat_ws(' ', last_name, first_name, middle_name) as name");
         if($pluck_id){
-           return Violator::where($full_name_query, $like, '%'.$search.'%'
+           return Violator::select('id', $full_name_query)->where('name', $like, '%'.$search.'%'
                 )->orWhere('license_number', $like, '%'.$search.'%'
                 )->pluck('id')->toArray();
         }
         return ViolatorResource::collection(
-            Violator::withCount('tickets')->where($full_name_query, $like, '%'.$search.'%'
+            Violator::select('*', $full_name_query)->withCount('tickets')->where('name', $like, '%'.$search.'%'
                 )->orWhere('license_number', $like, '%'.$search.'%'
                 )->orderBy('last_name', $order
                 )->orderBy('first_name', $order

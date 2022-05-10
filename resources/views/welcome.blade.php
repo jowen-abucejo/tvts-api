2c7655b38d4b906165618b26616d4557837c6d68
jowen-abucejo <jowennavalabucejo24@gmail.com>
2022-05-10 10:58:46 +0800

    -fixed margin for porivacy policy page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
 			<div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
 				<div class="grid grid-cols-1 md:grid-cols-1">
 					<div class="p-6">
-						<div class="ml-12">
+						<div class="ml-4">
 							<div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
 								<div class="flex items-center">
                                 
